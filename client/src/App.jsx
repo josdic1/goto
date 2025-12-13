aba488f0f79d7d3163bbdbcfe7165b2ae15fcd64
josdic1 <joshuadicker@gmail.com>
2025-12-13 15:25:32 -0500

    add request plumbing visualizer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { AuthProvider } from "./providers/AuthProvider";
 import { AppLayout } from "./components/layout/AppLayout";
-import { PlumbingVisualizer } from "./components/PlumbingVisualizer/PlumbingVisualizer";
+import { PlumbingVisualizer } from "./components/PlumbingVisualizer";
 
 function App() {
   return (
     <>
       <AuthProvider>
         <AppLayout />
-        <PlumbingVisualizer /> 
+        <PlumbingVisualizer />
       </AuthProvider>
     </>
   );
